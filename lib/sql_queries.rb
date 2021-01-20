aa65a348a70fdb1c0435b0ab854e8ae711da5a89
Josh <josh.handon@gmail.com>
2021-01-19 19:34:14 -0500

    commit tree ca0421a08cd23bc98841079d7ed611b5ce4bf464

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -9,18 +9,18 @@
 def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_title
   "SELECT projects.title, SUM(pledges.amount)
   FROM projects INNER JOIN pledges
-  ON project.id = pledges.project.id
+  ON projects.id = pledges.project_id
   GROUP BY projects.id
   ORDER BY projects.title;"
 end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
   "SELECT users.name, users.age, SUM(pledges.amount)
-  FROM projects
-  INNER JOIN pledges
-  ON projects.id = pledges.project_id
-  GROUP BY projects.id
-  ORDER BY projects.title;"
+  FROM users
+  JOIN pledges
+  ON users.id = pledges.user_id
+  GROUP BY users.name
+  ORDER BY users.name;"
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
